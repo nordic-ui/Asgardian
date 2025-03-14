2d7da68ffec0c8d8e1b83deebaea0a71299e2072
Kevin Østerkilde <kevin@oesterkilde.dk>
2025-03-14 23:18:11 +0100

    feat: cleanup

--- apps/docs/app/[[...mdxPath]]/page.tsx
+++ apps/docs/app/[[...mdxPath]]/page.tsx
@@ -4,6 +4,7 @@ import { useMDXComponents as getMDXComponents } from "../../mdx-components";
 
 export const generateStaticParams = generateStaticParamsFor("mdxPath");
 
+// @ts-expect-error - Typing the props later
 export const generateMetadata = async (props) => {
   const params = await props.params;
 
@@ -14,7 +15,9 @@ export const generateMetadata = async (props) => {
 
 const Wrapper = getMDXComponents({}).wrapper;
 
+// @ts-expect-error - Typing the props later
 const Page = async (props) => {
+  // eslint-disable-next-line react/prop-types
   const params = await props.params;
   const result = await importPage(params.mdxPath);
 
